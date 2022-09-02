752461d6abf9c889c7fd12b1f0350a2078b54833
Fastcaserogue19 <99422160+Fastcaserogue19@users.noreply.github.com>
2022-09-02 14:54:40 +0300

    Create 50. Pow(x, n).py

---
+++ leet_code_problems/50. Pow(x, n).py
@@ -0,0 +1,15 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if x==0:
+            return 0
+        if n==1:
+            return x
+        if n==0:
+            return 1
+        if n==-1:
+            return 1/x
+        if (n>0 or n<0) and n%2==0:
+            return self.myPow(x*x,n//2)
+        else:
+            return x*self.myPow(x*x,n//2)
+        
