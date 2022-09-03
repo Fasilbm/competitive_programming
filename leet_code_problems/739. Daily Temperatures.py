1fc79658bc692045881a1c14ef38bd6b9c8102a7
Fastcaserogue19 <99422160+Fastcaserogue19@users.noreply.github.com>
2022-09-03 15:47:51 +0300

    Create 739. Daily Temperatures.py

---
+++ leet_code_problems/739. Daily Temperatures.py
@@ -0,0 +1,16 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        answer=[0]*len(temperatures)
+        stack=[] 
+        x=len(temperatures)-1
+        for i in range(len(temperatures)):
+           
+                while stack and temperatures[x]>=temperatures[stack[-1]]:
+                    stack.pop()
+            
+                if stack and temperatures[x]<temperatures[stack[-1]]: 
+                    answer[x]=stack[-1]-x
+        
+                stack.append(x)
+                x-=1
+        return answer
