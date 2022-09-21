3372d36a9e7e22545597138fb179576a0af24597
Fastcaserogue19 <99422160+Fastcaserogue19@users.noreply.github.com>
2022-09-21 14:31:12 +0300

    Update 724. Find Pivot Index.py

--- leet_code_problems/724. Find Pivot Index.py
+++ leet_code_problems/724. Find Pivot Index.py
@@ -2,15 +2,13 @@ class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
         pivot=[]
         pref=[0]*len(nums)
-        left=0
-        right=0
         for j in range(1,len(nums)+1):
             pref[j-1]=nums[j-1]+pref[j-2]
         for i in range(0,len(nums)):
-            if i==0 and (left==pref[len(nums)-1]-pref[0]):
+            if i==0 and (0==pref[len(nums)-1]-pref[0]):
                 pivot.append(i)
                 continue
-            elif i==len(nums)-1 and (right==pref[i-1]):
+            elif i==len(nums)-1 and (0==pref[i-1]):
                 pivot.append(i)
                 continue
             elif i!=0 and (pref[i-1]==pref[len(nums)-1]-pref[i]):
