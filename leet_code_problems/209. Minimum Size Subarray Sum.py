94a0311c761c9b6dbdaac6fc92fbca0c22dd751e
Fastcaserogue19 <99422160+Fastcaserogue19@users.noreply.github.com>
2022-09-23 11:24:22 +0300

    Create 209. Minimum Size Subarray Sum.py

---
+++ leet_code_problems/209. Minimum Size Subarray Sum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        start=0
+        curr_sum=0
+        res=len(nums)+1
+        for i in range(len(nums)):
+            curr_sum+=nums[i]
+            while curr_sum>=target:
+                res=min(i-start+1,res)
+                curr_sum-=nums[start]
+                start+=1
+        return res if res<len(nums)+1 else 0
+                
+            
