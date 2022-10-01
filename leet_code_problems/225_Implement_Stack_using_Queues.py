1d908138e3ddc93101f20925f4b67f9ca6b1faa1
Fastcaserogue19 <99422160+Fastcaserogue19@users.noreply.github.com>
2022-10-01 11:06:54 +0300

    Update 225_Implement_Stack_using_Queues.py

--- leet_code_problems/225_Implement_Stack_using_Queues.py
+++ leet_code_problems/225_Implement_Stack_using_Queues.py
@@ -1,38 +1,24 @@
 class MyStack:
 
     def __init__(self):
-        self.list1=[]
-        self.list2=[]
-        
-
-    def push(self,x: int) -> None:
-        self.list1.append(x)
-        
-
+        self.queue=deque()
+    def push(self, x: int) -> None:
+        self.queue.append(x)
     def pop(self) -> int:
-                for i in range(len(self.list1)-2):
-                    for j in range(len(self.list2)-1):
-                        temp=self.list1[i]
-                        self.list1[i]=self.list2[j]
-                        self.list2[j]=temp
-                return self.list1.pop()
-                self.list1=self.list2.copy()
-            
+        for i in range(len(self.queue)-1):
+            self.queue.append(self.queue.popleft())
+        return self.queue.popleft()
+        
     def top(self) -> int:
-            
-            return self.list1[-1]
-
+        return self.queue[-1]
     def empty(self) -> bool:
-        if len(self.list1)==0:
-            return True
-        else:
-            return False
-    
-'''
-obj = MyStack()
-obj.push(x)
-param_2 = obj.pop()
-param_3 = obj.top()
-param_4 = obj.empty()
+        return len(self.queue)==0
+        
 
-'''        
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
