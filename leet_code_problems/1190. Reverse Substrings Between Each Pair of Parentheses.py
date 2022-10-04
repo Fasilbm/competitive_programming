011e9539313e3bbad509a3104e3508b69bdd4124
Fastcaserogue19 <99422160+Fastcaserogue19@users.noreply.github.com>
2022-10-04 10:34:30 +0300

    Create 1190. Reverse Substrings Between Each Pair of Parentheses.py

---
+++ leet_code_problems/1190. Reverse Substrings Between Each Pair of Parentheses.py
@@ -0,0 +1,16 @@
+class Solution:
+    def reverseParentheses(self, s: str) -> str:
+        stack=[]
+        for i in s:
+            if i!=')':
+                stack.append(i)
+            else:
+                reverse=[]
+                while stack[-1]!='(':
+                    reverse.append(stack.pop())
+                stack.pop()
+                stack.extend(reverse)
+        
+        return "".join(stack)
+    # Time complexity O(n^2)
+    # Space complexity O(n)
