36a6a4c13bd160d4f31648146f0c346f1e81ae45
Fastcaserogue19 <99422160+Fastcaserogue19@users.noreply.github.com>
2022-12-22 19:31:16 +0300

    Update 1491. Average Salary Excluding the Minimum and Maximum Salary.py

--- leet_code_problems/1491. Average Salary Excluding the Minimum and Maximum Salary.py
+++ leet_code_problems/1491. Average Salary Excluding the Minimum and Maximum Salary.py
@@ -1,8 +1,12 @@
 class Solution:
     def average(self, salary: List[int]) -> float:
 
-        salary.sort()
-        left=0
-        right=len(salary)-1
-        average=(sum(salary)-salary[left]-salary[right])/(len(salary)-2)
+        # salary.sort()
+        # left=0
+        # right=len(salary)-1
+        # average=(sum(salary)-salary[left]-salary[right])/(len(salary)-2)
+        # return average
+        minimum=min(salary)
+        maximum=max(salary)
+        average=(sum(salary)-maximum-minimum)/(len(salary)-2)
         return average
