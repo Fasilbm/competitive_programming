393e1f73dacb5fe4181421f7c0cba4d63dafbc98
Fastcaserogue19 <99422160+Fastcaserogue19@users.noreply.github.com>
2023-01-22 03:15:11 +0300

    Update 2089_Find_Target_Indices_After_Sorting _Array.py

--- leet_code_problems/2089_Find_Target_Indices_After_Sorting _Array.py
+++ leet_code_problems/2089_Find_Target_Indices_After_Sorting _Array.py
@@ -1,12 +1,8 @@
 class Solution:
     def targetIndices(self, nums: List[int], target: int) -> List[int]:
-        list=[]
-        for i in range(len(nums)-1):
-            for j in range(i+1,len(nums)):
-                if nums[j]<nums[i]:
-                    nums[i],nums[j]=nums[j],nums[i]
-        for k in range (len(nums)):
-            if target==nums[k]:
-                 list.append(k)
-        return list
-           
+        ans=[]
+        nums.sort()
+        for i,j in enumerate(nums):
+            if j==target:
+                ans.append(i)
+        return ans
