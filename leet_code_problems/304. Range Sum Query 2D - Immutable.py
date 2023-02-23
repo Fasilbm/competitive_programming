88da75145b66156440136520494af6ce53a1f98b
Fastcaserogue19 <99422160+Fastcaserogue19@users.noreply.github.com>
2023-02-23 22:51:55 +0300

    Update 304. Range Sum Query 2D - Immutable.py

--- leet_code_problems/304. Range Sum Query 2D - Immutable.py
+++ leet_code_problems/304. Range Sum Query 2D - Immutable.py
@@ -1,29 +1,29 @@
 class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
-        row,col=len(matrix),len(matrix[0])
-        self.prefix_matrix=[[0]*(col+1)for i in range(row+1)]
-        for r in range(row):
-            prefix=0
-            for c in range(col):
-                prefix+=matrix[r][c]
-                above=self.prefix_matrix[r][c+1]
-                self.prefix_matrix[r+1][c+1]=prefix+above
-        
+        self.matrix = matrix
+        n=len(self.matrix)
+        m=len(self.matrix[0])
+        self.prefix=[ [0 for j in range(m+1)] for i in range(n+1)]
+
+
+
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+
+                self.prefix[i][j]=self.matrix[i-1][j-1]+self.prefix[i][j-1]+self.prefix[i-1][j]-self.prefix[i-1][j-1]
+
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        row1,col1,row2,col2=row1+1,col1+1,row2+1,col2+1
-        above=self.prefix_matrix[row1-1][col2]
-        left=self.prefix_matrix[row2][col1-1]
-        bottom_right=self.prefix_matrix[row2][col2]
-        top_left=self.prefix_matrix[row1-1][col1-1]
-        return bottom_right-above-left+top_left
-    # Time complexity O(n^2)
-    # Space complexity O(n^2)
-    
+
+        return self.prefix[row2][col2]-self.prefix[row2][col1-1]-self.prefix[row1+1][col2+1]+self.prefix[row1][col1]
+
+
+      
+
+  
+
         
-         
-         
 
 
 # Your NumMatrix object will be instantiated and called as such:
