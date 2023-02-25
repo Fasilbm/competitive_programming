005edbac9a16e7260fdad3330e76e09f08ef8761
Fastcaserogue19 <99422160+Fastcaserogue19@users.noreply.github.com>
2023-02-25 07:42:54 +0300

    Create 238. Product of Array Except Self.py

--- leet_code_problems/238. Product of Array Except Self.py
+++ leet_code_problems/238. Product of Array Except Self.py
@@ -1,20 +1,24 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        result=[0 for i in range(len(nums))]
-        prefix=[0 for i in range(len(nums))]
-        postfix=[0 for i in range(len(nums))]
-        prefix[0]=nums[0]
-        postfix[len(nums)-1]=nums[len(nums)-1]
-        for i in range(1,len(nums)):
-            prefix[i]=prefix[i-1]*nums[i]
-            prefix.append(prefix[i])
-        for i in range(len(nums)-2,-1,-1):
-            postfix[i]=nums[i]*postfix[i+1]
-            postfix.append(postfix[i])
-        result[0]=postfix[1]
-        result[len(nums)-1]=prefix[len(nums)-2]
-        for j in range(1,len(nums)-1):
-            result[j]=prefix[j-1]*postfix[j+1]
-        return result
-    #Time complexity O(n)
-    #Space complexity O(n)
+
+
+        pre_mul=[1]
+        post_mul=[1]*(len(nums)+1)
+        ans=[]
+
+        for i in range(len(nums)):
+
+            pre_mul.append(pre_mul[-1]*nums[i])
+
+        for j in range(len(nums)-1,-1,-1):
+
+            post_mul[j]=post_mul[j+1]*nums[j]
+
+        # print(pre_mul)
+        # print(post_mul)
+
+        for i in range(len(nums)):
+
+                ans.append(post_mul[i+1]*pre_mul[i])
+
+        return (ans)
