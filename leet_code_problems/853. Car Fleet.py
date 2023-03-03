b57d4858e7ffbe6d7970a15e9522aebfc102d89c
Fastcaserogue19 <99422160+Fastcaserogue19@users.noreply.github.com>
2023-03-03 07:00:32 +0300

    Update 853. Car Fleet.py

--- leet_code_problems/853. Car Fleet.py
+++ leet_code_problems/853. Car Fleet.py
@@ -7,3 +7,32 @@ class Solution:
             if len(stack)>=2 and stack[-1]<=stack[-2]:
                 stack.pop()
         return len(stack)
+    
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+
+        pair=[]
+        time=[]
+
+        for i,j in enumerate(position):
+
+            pair.append([j,speed[i]])
+
+        pair.sort()
+
+        for i,j in pair:
+
+            time.append((target-i)/j)
+
+        stack=[time[-1]]
+
+        for i in range(len(time)-2,-1,-1):
+
+            stack.append(time[i])
+
+            if stack and stack[-2]>=stack[-1]:
+
+                stack.pop()
+
+        return len(stack)
+            
