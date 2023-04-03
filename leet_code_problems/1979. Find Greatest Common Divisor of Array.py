fb8baf78a7f5c7c0380cb364e18a81979ec6a592
Fastcaserogue19 <99422160+Fastcaserogue19@users.noreply.github.com>
2023-04-03 10:46:25 +0300

    Create 1979. Find Greatest Common Divisor of Array.py

---
+++ leet_code_problems/1979. Find Greatest Common Divisor of Array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findGCD(self, nums: List[int]) -> int:
+
+        nums.sort()
+
+        a=nums[0]
+        b=nums[-1]
+        def gcd(a, b):
+            if b == 0:
+                return (a)
+            return gcd(b, a % b)
+        return gcd(a,b)
+
+
+        
